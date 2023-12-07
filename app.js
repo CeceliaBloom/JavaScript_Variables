782473a8eb180a1328bac76aa4d957ab9ebb0325
Cecelia Bloom <crbloom01@gmail.com>
2023-12-07 10:20:41 -0500

    Finished JS exercise

--- app.js
+++ app.js
@@ -3,8 +3,38 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+            //Exercise 1
+let lastName="Bloom";
+var age=23;
 
-// Exercise 2
+            //Exercise 2
+const firstName="Cecelia";
 
-// Exercise 3
+            //Exercise 3
+let language="JavaScript";
+let createdYear=1995;
+let isCaseSensitive=true;
+
+console.log(`${language} was first relased in ${createdYear}.`)
+
+            //Exercise 4
+let product="T-shirt";
+let price=19.99;
+let isOnSale=false;
+let salePercentage=15;
+let stock=0;
+let inStock=false;
+let selectedSize="M";
+
+console.log(`${product} in size ${selectedSize} is ${price} with ${stock} in stock.`);
+
+            //Exercise 5
+let title="Name of the Wind";
+let author="Patrick Rothfuss"; 
+let pageCount=722;
+let bookmark=456;
+let hasRead=true;
+
+
+
+
